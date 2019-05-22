ff0d4981379b68f9d99f00dfa16cfb3ba67b0f16
watchmakert <carrasquilla1999@gmail.com>
2019-05-22 10:43:34 -0500

    tercer commit

--- prueba/Controllers/PrestamosController.cs
+++ prueba/Controllers/PrestamosController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Busisnes.PrestamosBusisness.Interfaces;
+using Data.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +13,36 @@ namespace prueba.Controllers
     [ApiController]
     public class PrestamosController : ControllerBase
     {
+        private readonly IPrestamosServices _IPrestamosServices;
+
+        public PrestamosController(IPrestamosServices _IPrestamosServices)
+        {
+            this._IPrestamosServices = _IPrestamosServices;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Prestamo>> Get()
+        {
+            List<Prestamo> prestamo = _IPrestamosServices.GetPrestamos();
+            return prestamo;
+        }
+        [HttpGet("{id}", Name = "Get")]
+        public ActionResult<Prestamo> Get(int id)
+        {
+            Prestamo prestamo = _IPrestamosServices.ConsultarPrestamo(id);
+            return prestamo;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Prestamo prestamo)
+        {
+            _IPrestamosServices.AgregarPrestamo(prestamo.Fechainicio, prestamo.Fechafin,prestamo.IdPropietario, prestamo.IdPrestador, prestamo.Idaeronave);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _IPrestamosServices.BorrarPrestamo(id);
+        }
     }
 }
--- prueba/Controllers/RutasController.cs
+++ prueba/Controllers/RutasController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Busisnes.RutasBusisness.Interfaces;
+using Data.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +13,36 @@ namespace prueba.Controllers
     [ApiController]
     public class RutasController : ControllerBase
     {
+        private readonly IRutaServices _IRutasServices;
+
+        public RutasController(IRutaServices _IRutasServices)
+        {
+            this._IRutasServices = _IRutasServices;
+        }
+        
+        [HttpGet]
+        public ActionResult<List<Ruta>> Get()
+        {
+            List<Ruta> rutas = _IRutasServices.GetRutas();
+            return rutas;
+        }
+        [HttpGet("{id}", Name = "Get")]
+        public ActionResult<Ruta> Get(int id)
+        {
+            Ruta ruta = _IRutasServices.ConsultarRuta(id);
+            return ruta;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Ruta ruta)
+        {
+            _IRutasServices.AgregarRutas(ruta.Fechainicio, ruta.Fechafin, ruta.IdAeronave, ruta.IdOrigen, ruta.IdDestino);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _IRutasServices.BorrarRuta(id);
+        }
     }
 }
